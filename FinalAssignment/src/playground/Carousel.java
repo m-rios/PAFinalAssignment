e593598db78c799da6f76a703084273b2f9e19a8
Mario <mrm994@gmail.com>
2014-04-22 16:48:59 +0200

    Carousel is finished
    Now it waits if its full, notifies all when gets empty and doesn’t
    start until 5 children are mounted

--- FinalAssignment/src/playground/Carousel.java
+++ FinalAssignment/src/playground/Carousel.java
@@ -23,42 +23,43 @@ public class Carousel extends Attraction {
     public Carousel() {
         super();
         super.playingQueue = new ArrayList<>(5);
-        barrier = new CyclicBarrier(5,new Runnable() {
-
-            @Override
-            public void run() {
-                try {
-                    sleep((long)5000);
-                } catch (InterruptedException ex) {
-                    Logger.getLogger(Carousel.class.getName()).log(Level.SEVERE, null, ex);
-                }
-            }
-        });
+        barrier = new CyclicBarrier(5);
     }
 
     @Override
     public void use(Child child) {
         enter(child);
-       /* try {
+        try {
             sleep((long) 5000);
         } catch (InterruptedException ex) {
             Logger.getLogger(Carousel.class.getName()).log(Level.SEVERE, null, ex);
-        }*/
+        }
         leave(child);
     }
 
     private synchronized void enter(Child child) {
         super.waitingQueue.add(child);
+        //wait while full or not first in waiting queue
+        while (playingQueue.size() == 5 && waitingQueue.indexOf(child) != 0) {
+            try {
+                wait();
+            } catch (InterruptedException ex) {
+            }
+        }
+        super.waitingQueue.remove(child);
+        super.playingQueue.add(child);
+        //wait until all 5 sits are occupied.
         try {
             barrier.await();
         } catch (InterruptedException | BrokenBarrierException ex) {
             Logger.getLogger(Carousel.class.getName()).log(Level.SEVERE, null, ex);
         }
-        super.waitingQueue.remove(child);
-        super.playingQueue.add(child);
     }
 
     private synchronized void leave(Child child) {
-
+        playingQueue.remove(child);
+        if (playingQueue.isEmpty()) {
+            notifyAll();
+        }
     }
 }
