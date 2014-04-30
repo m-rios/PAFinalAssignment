b899eaa96da566e95ea99dba7a6746042a8a01af
Mario <mrm994@gmail.com>
2014-04-30 18:03:21 +0200

    now is fully working

--- FinalAssignment/src/playground/Carousel.java
+++ FinalAssignment/src/playground/Carousel.java
@@ -19,7 +19,29 @@ import javax.swing.JTextArea;
  * @author mario
  */
 public class Carousel extends Attraction {
-
+    
+    private class Awaker extends Thread{
+        Carousel c;
+        public Awaker(Carousel c){
+            this.c = c;
+            start();
+        }
+        @Override
+        public void run(){
+            while(true){
+                if (c.threadsWaiting()){
+                    c.awake();
+                }
+                try {
+                    sleep(100);
+                } catch (InterruptedException ex) {
+                    Logger.getLogger(Carousel.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        }
+    }
+    
+    Awaker awaker;
     final CyclicBarrier barrier;
     JProgressBar indicator;
 
@@ -29,31 +51,23 @@ public class Carousel extends Attraction {
         barrier = new CyclicBarrier(5);
         this.indicator = progressBar;
         indicator.setMaximum(10);
+        awaker = new Awaker(this);
     }
 
     @Override
     public void use(Child child) {
         enter(child);
-        synchronized (this) {
-            notifyAll(); //esto es una puta cutrez
-        }
         try {
             barrier.await();
-            if (indicator.getPercentComplete() == 0) {
-                int progress = 5;
-                while (progress < 10) {
-                    indicator.setValue(progress);
-                    progress += 5;
-                    // Update the progress bar
-                    sleep(500);
-                }
-            } else {
-                sleep((long) 5000);
-            }
+            sleep((long) 5000);
         } catch (InterruptedException | BrokenBarrierException ex) {
             Logger.getLogger(Carousel.class.getName()).log(Level.SEVERE, null, ex);
         }
         leave(child);
+    }
+    
+    public synchronized void awake(){
+        notifyAll();
     }
 
     private synchronized void enter(Child child) {
@@ -70,19 +84,22 @@ public class Carousel extends Attraction {
         updateWaitView();
         super.playingQueue.add(child);
         updatePlayView();
-        //wait until all 5 sits are occupied.
-        /*try {
-         barrier.await();
-         } catch (InterruptedException | BrokenBarrierException ex) {
-         Logger.getLogger(Carousel.class.getName()).log(Level.SEVERE, null, ex);
-         }*/
     }
 
     private synchronized void leave(Child child) {
         playingQueue.remove(child);
         updatePlayView();
-        if (playingQueue.isEmpty()) {
-            notifyAll();
+        notifyAll();
+    }
+
+    public boolean threadsWaiting() {
+        synchronized (waitingQueue) {
+            for (int i = 0; i < waitingQueue.size(); i++) {
+                if (waitingQueue.get(i).getState() == Thread.State.WAITING) {
+                    return true;
+                }
+            }
         }
+        return false;
     }
 }
